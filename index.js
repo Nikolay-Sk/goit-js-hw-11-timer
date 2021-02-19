c53a112b58671e4cdc6b63ec0e1d739244acc184
Nikolay-Sk <69104529+Nikolay-Sk@users.noreply.github.com>
2021-02-19 19:47:58 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+    this.updateClockface();
+    this.refs = {
+      days: document.querySelector('[data-value="days"]'),
+      hours: document.querySelector('[data-value="hours"]'),
+      mins: document.querySelector('[data-value="mins"]'),
+      secs: document.querySelector('[data-value="secs"]'),
+    };
+  }
+  updateClockface() {
+    setInterval(() => {
+      const nowDate = Date.now();
+      const time = this.targetDate - nowDate;
+
+      const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+      const hours = pad(
+        Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+      );
+      const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+      const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+
+      this.refs.days.textContent = days;
+      this.refs.hours.textContent = hours;
+      this.refs.mins.textContent = mins;
+      this.refs.secs.textContent = secs;
+
+      function pad(value) {
+        return String(value).padStart(2, '0');
+      }
+    }, 1000);
+  }
+}
+
+new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Jul 17, 2021'),
+});
